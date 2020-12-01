55128e5519c1d618e552237261a780041dab5139
ReenyeLima <52282158+ReenyeLima@users.noreply.github.com>
2020-11-30 22:19:43 -0300

    Load JSON
    Atualizacao no Init PHP (Nova estrutura de pastas e arquivos base)
    Implementado o comando Load JSON (Ciriacao dos codigos baseado em um objeto json)

--- extension.js
+++ extension.js
@@ -5,9 +5,11 @@ const path = require("path");
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+const folderPath = vscode.workspace.rootPath + path.sep;
+
 function activate(context) {	
-	const folderPath = vscode.workspace.rootPath + path.sep;
-	const paths = ["src", "view", `src${path.sep}controll`, `src${path.sep}domain`, `view${path.sep}home.html`,"index.html"];
+	const paths = ["src", "view", `src${path.sep}controll`, `src${path.sep}domain`, `view${path.sep}home.html`,"index.html", "base.json"];
 
 	let disposable = vscode.commands.registerCommand('mvc-php.initPhp', function () {		
 		if (!folderPath) {
@@ -19,16 +21,29 @@ function activate(context) {
 
 		for(let i = 0; i < paths.length; i++){
 			if(!fs.existsSync(folderPath + paths[i])) {
-				if(!paths[i].includes("html")) {
+				if(!paths[i].includes(".")) {
 					fs.mkdir(path.join(folderPath, paths[i]), (err) => {
 						if(err){
 							vscode.window.showErrorMessage(err.message);
 							return;
 						}
-					})	
+					});
+					if(paths[i].includes("controll")) {
+						let controllFolders = ["routes", "process"];
+						controllFolders.forEach((folder) => {
+							folder = paths[i] + path.sep + folder;
+							fs.mkdir(path.join(folderPath, folder), (err) => {
+								if(err){
+									vscode.window.showErrorMessage(err.message);
+									return;
+								}
+							});
+						})
+					}
 				}else{
 					if(paths[i].includes("index")) content = generateIndex();
 					else if(paths[i].includes("home")) content = generateHome();
+					else if(paths[i].includes("json")) content = generateBasejson();
 					fs.writeFile(path.join(folderPath, paths[i]), content, err => {
 						if (err) {
 							return vscode.window.showErrorMessage(
@@ -181,6 +196,24 @@ function activate(context) {
 
 	context.subscriptions.push(disposable);
 
+	disposable = vscode.commands.registerCommand('mvc-php.loadJson', () => {
+		let pathFile = folderPath + "base.json";
+		fs.readFile(pathFile, 'utf8', function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			let obj = JSON.parse(data);
+
+			createConfigs(obj.Connection);
+			createConection(obj.Database);
+			JSONtoClass(obj.Class);
+			JSONtoProcess(obj.Class);
+			createRoutes(obj.Class);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+
 	/*context.subscriptions.push(vscode.commands.registerTextEditorCommand("mvc-php.sayHello", (editor, edit) => {
 		
 	}));*/
@@ -189,6 +222,43 @@ function activate(context) {
 exports.activate = activate;
 
 function deactivate() {}
+
+function generateBasejson() {
+	return (
+		`/*{
+	"Connection" : {
+		"Headers" : {
+			"Content-type" : "application/json; charset=UTF-8",
+			"Access-Control-Allow-Origin" : "http://localhost/",
+			"Access-Control-Allow-Methods" : "GET, PUT, POST, DELETE"
+		}
+	},
+	"Database" : {
+		"host" : "localhost",
+		"port" : "3306",
+		"dbname" : "database_name",
+		"user" : "root",
+		"password" : ""
+	},
+	"Class" : [
+		{
+			"name" : "className1",
+			"attributes" : [
+				"attr1",
+				"attr2"
+			]
+		},
+		{
+			"name" : "className2",
+			"attributes" : [
+				"attr1",
+				"attr2"
+			]
+		}
+	]
+}*/`
+	);
+}
 
 function generateIndex() {
   return (
@@ -259,6 +329,216 @@ function generateClass(name, data) {
 	text += `\n\t}`;
 	
 	return text;
+}
+
+function createConection(data) {	
+	let dir = folderPath + "src" + path.sep + "domain";
+	let content = `<?php`;
+	content += `\n\n\tclass Connection{`;
+	content += `\n\t\tpublic static $instance;`;
+	content += `\n\n\t\tpublic static function getInstance(){`;
+	content += `\n\t\t\t$url = "mysql:host=${data.host};port=${data.port};dbname=${data.dbname};";`;
+	content += `\n\n\t\t\t$user = "${data.user}";`;
+	content += `\n\n\t\t\t$password = "${data.password}";`;
+	content += `\n\n\t\t\t$config = array(PDO::ATTR_PERSISTENT => true,PDO::ATTR_CASE => PDO::CASE_LOWER);`;
+	content += `\n\n\t\t\tif (!isset(self::$instance)) {`;
+	content += `\n\t\t\t\tself::$instance = new PDO($url,$user,$password,$config);`;
+	content += `\n\n\t\t\t\tself::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);`;
+	content += `\n\t\t\t}`;
+	content += `\n\n\t\t\treturn self::$instance;`;
+	content += `\n\t\t}`;
+	content += `\n\t}`;
+
+	fs.writeFile(path.join(dir, "connection.php"), content, err => {
+		if (err) {
+			return vscode.window.showErrorMessage(
+				"Falha na criação do arquivo HTML\r\n" + err.message
+			);
+		}
+		/*vscode.window.showInformationMessage(
+			"HTML criado com sucesso"
+		);*/
+	});
+}
+
+function createConfigs(connection) {
+	let dir = folderPath + "src" + path.sep + "controll" + path.sep + "routes";
+
+	let verbs;
+
+	let headers = Object.entries(connection.Headers);
+	
+	let content = `<?php\n`;
+
+	headers.forEach((header) =>{
+		content += `\n\theader("${header[0]}: ${header[1]}");`;
+		if(header[0] == "Access-Control-Allow-Methods") {
+			verbs = header[1].replace(/ /g, "").split(",");
+		}
+	});
+
+	verbs.forEach((verb) => {
+		if((verb.toUpperCase() != "POST") && (verb.toUpperCase() != "GET")){
+			content += `\n\n\t$_${verb.toUpperCase()} = array();`;
+			content += `\n\tif (!strcasecmp($_SERVER['REQUEST_METHOD'], '${verb.toUpperCase()}')) {`;
+			content += `\n\t\tparse_str(file_get_contents('php://input'), $_${verb.toUpperCase()});`;
+			content += `\n\t}`;
+		}
+	});
+
+	fs.writeFile(path.join(dir, "configs.php"), content, err => {
+		if (err) {
+			return vscode.window.showErrorMessage(
+				"Falha na criação do arquivo HTML\r\n" + err.message
+			);
+		}
+	});
+}
+
+function JSONtoClass(arr) {	
+	let dir = folderPath + "src" + path.sep + "domain";
+
+	arr.forEach((obj) => {		
+		let className = obj.name[0].toUpperCase() + obj.name.substr(1);
+
+		let content = `<?php`;
+		content += `\n\n\tclass ${className} {`;
+		
+		obj.attributes.forEach((attribute) => {
+			content += `\n\t\tvar $${attribute};`;
+		})
+
+		obj.attributes.forEach((attribute) => {
+			let attributeName = attribute[0].toUpperCase() + attribute.substr(1);
+
+			content += `\n\n\t\tfunction get${attributeName}(){`;
+			content += `\n\t\t\treturn $this->${attribute};`;
+			content += `\n\t\t}`;
+			content += `\n\t\tfunction set${attributeName}($${attribute}){`;
+			content += `\n\t\t\t$this->${attribute} = $${attribute};`;
+			content += `\n\t\t}`;
+		})
+		
+		content += `\n\t}\n`;
+		content += `\n\tclass ${className}DAO {`;
+
+		let crud = ["create", "read", "update", "delete"];
+
+		crud.forEach((item) => {
+			let param = "";
+			let query = "";
+			let condition = `\n\n\t\t\t\tif(Connection::getInstance()->exec($query) >= 1){`;
+
+			switch(item) {
+				case "create":
+					param = `$${obj.name}`;
+					query = `\n\t\t\t\t$query = "INSERT INTO table_name (column1, column2) VALUES (value1, value2)";`;
+					break;
+				case "read":
+					query = `\n\t\t\t\t$query = "SELECT column1, column2 FROM table_name WHERE condition";`;
+					break;
+				case "update":
+					condition = `\n\n\t\t\t\t$status = Connection::getInstance()->prepare($query);`; 
+					condition += `\n\n\t\t\t\tif($status->execute()){`; 
+					query = `\n\t\t\t\t$query = "UPDATE table_name SET column1 = value1, column2 = value2 WHERE condition";`;
+					break;
+				case "delete":
+					query = `\n\t\t\t\t$query = "DELETE FROM table_name WHERE condition";`;
+					break;
+			}
+
+			content += `\n\t\tfunction ${item}(${param}) {`;
+			content += `\n\t\t\t$result = array();`;
+			content += `\n\n\t\t\ttry {`;
+			content += query;
+			content += `\n\n\t\t\t\t$con = new Connection();`;
+			content += condition;
+			content += `\n\t\t\t\t}`;
+			content += `\n\t\t\t}catch(PDOException $e) {`;
+			content += `\n\t\t\t}`;
+			content += `\n\n\t\t\treturn result;`;
+			content += `\n\t\t}\n`;
+		});
+		
+		content += `\t}\n`;
+
+		
+		fs.writeFile(path.join(dir, `${obj.name}.php`), content, err => {
+			if (err) {
+				return vscode.window.showErrorMessage(
+					"Falha na criação do arquivo HTML\r\n" + err.message
+				);
+			}
+		});
+	});
+}
+
+function createRoutes(arr) {
+	let dir = folderPath + "src" + path.sep + "controll" + path.sep + "routes";
+
+	arr.forEach((obj) => {	
+		let className = obj.name[0].toUpperCase() + obj.name.substr(1);
+		let objProc = `${obj.name[0]}p`;
+		
+		let content = `<?php`;
+		content += `\n\n\trequire("../process/process.${obj.name}.php");`;
+		content += `\n\n\tinclude("configs.php");`;
+		content += `\n\n\t$${objProc} = new ${className}Process();`;
+		content += `\n\n\tswitch($_SERVER['REQUEST_METHOD']) {`;
+		content += `\n\t\tcase "GET":`;
+		content += `\n\t\t\t$${objProc}->doGet($_GET);`;
+		content += `\n\t\t\tbreak;`;
+		content += `\n\n\t\tcase "POST":`;
+		content += `\n\t\t\t$${objProc}->doPost($_POST);`;
+		content += `\n\t\t\tbreak;`;
+		content += `\n\n\t\tcase "PUT":`;
+		content += `\n\t\t\t$${objProc}->doPut($_PUT);`;
+		content += `\n\t\t\tbreak;`;
+		content += `\n\n\t\tcase "DELETE":`;
+		content += `\n\t\t\t$${objProc}->doDelete($_DELETE);`;
+		content += `\n\t\t\tbreak;`;
+		content += `\n\t}`;
+
+		fs.writeFile(path.join(dir, `route.${obj.name}.php`), content, err => {
+			if (err) {
+				return vscode.window.showErrorMessage(
+					"Falha na criação do arquivo HTML\r\n" + err.message
+				);
+			}
+		});		
+	});
+}
+
+function JSONtoProcess(arr) {
+	let dir = folderPath + "src" + path.sep + "controll" + path.sep + "process";
+	let requests = ["doGet", "doPost", "doPut", "doDelete"];
+
+	arr.forEach((obj) => {		
+		let className = obj.name[0].toUpperCase() + obj.name.substr(1);
+		let objDao = `${obj.name[0]}d`;
+		
+		let content = `<?php`;
+		content += `\n\n\trequire("../../domain/connection.php");`;
+		content += `\n\trequire("../../domain/${className}.php");`;
+		content += `\n\n\t$${objDao} = new ${className}DAO();`;
+		content += `\n\n\tclass ${className}Process {`;
+		requests.forEach((request) => {
+			content += `\n\t\tfunction ${request}($arr){`;
+			content += `\n\t\t\t$sucess = "use to result to DAO";`;
+			content += `\n\t\t\thttp_response_code(202);`;
+			content += `\n\t\t\techo json_encode($sucess);`;
+			content += `\n\t\t}\n`;
+		});
+		content += `\t}`;
+
+		fs.writeFile(path.join(dir, `process.${obj.name}.php`), content, err => {
+			if (err) {
+				return vscode.window.showErrorMessage(
+					"Falha na criação do arquivo HTML\r\n" + err.message
+				);
+			}
+		});	
+	});
 }
 
 function getCurrentWorkspaceFolder() {
